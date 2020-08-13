71326b60599cf3df270ad97d5e7eb845dfcb37b7
Joona Savela <joona.savela@aalto.fi>
2020-08-13 19:13:59 +0300

    add simple version

--- booking_listener.js
+++ booking_listener.js
@@ -1,19 +1,9 @@
 
-function isBookingCompleteEvent(e) {
-  return e.event.localeCompare("bookingCompletion") === 0;
-}
-
 window.addEventListener('message', function(e) {
-  var event_type = "";
+  var event_type = e.event;
 
-  if (isBookingCompleteEvent(e)) {
-    event_type = "bookingCompletion";
-  }
-
-  if (event_type.localeCompare("") !== 0) {
-    window.dataLayer.push({
-      'event': event_type,
-      'original_event': e
-    });
-  }
+  window.dataLayer.push({
+    'event': event_type,
+    'original_event': e
+  });
 });
